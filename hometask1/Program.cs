276a733d6ea85b33c6a14443eab842c97dd87c9c
Kirill <pilot-stalker@mail.ru>
2022-09-05 09:27:09 +0300

    перезаписал новый код первого дз

--- hometask1/Program.cs
+++ hometask1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Пожалуйста Введите число");
+ double v;
+ double z;
+double o;
+
+v = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите сптепень");
+z=Convert.ToDouble(Console.ReadLine());
+
+o = Math.Pow(v,z);
+
+Console.WriteLine("вот ваш ответ" +v+ "в степени" +z+ "равно" +o+ "!" ) ;
